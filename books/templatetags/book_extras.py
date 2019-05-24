035b2bc7cf672902cacfac12cae11ee561598b26
Vikram Durai <vikramvdurai@gmail.com>
2019-05-24 06:16:50 +0000

    oof v2; restored certs

--- books/templatetags/book_extras.py
+++ books/templatetags/book_extras.py
@@ -14,9 +14,5 @@ def books_by_author(a):
     publications = a.books()
     books = []
     for i in publications:
-<<<<<<< HEAD
         books.extend(list(Book.objects.filter(publication=i).all()))
-=======
-        books.extend(list(Books.objects.filter(publication=i).all()))
->>>>>>> 8ff3536185f64245a4e0752f97a39bc411b36ff9
     return books
