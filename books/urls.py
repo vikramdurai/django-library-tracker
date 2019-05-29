0c303b56375ac57c965fddcc6a238666ac43c1f0
Vikram Durai <vikramvdurai@gmail.com>
2019-05-29 13:58:41 +0530

    css

--- books/urls.py
+++ books/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^search/', views.search, name="search"),
-    url(r'^borrowed', views.view_books, name="borrowed_books"),
+    url(r'^checkin-a-book', views.view_books, name="borrowed_books"),
     url(r'^titles/(?P<slug>[-\w\d]+)/book/(?P<acc>[-\w\d]+)/$',
         views.view_one_book, name="titles"),
     url(r'^extend/(?P<slug>[-\w\d]+)/book/(?P<acc>[-\w\d]+)/$',
