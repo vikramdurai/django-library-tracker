baa99255414666a3d2e96c7aea550956fb3b3494
Vikram Durai <vikramvdurai@gmail.com>
2019-11-03 18:17:14 +0530

    patch

--- books/management/commands/import.py
+++ books/management/commands/import.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import MultipleObjectsReturned
 from datetime import datetime, timedelta
 from books.models import *
 import csv
